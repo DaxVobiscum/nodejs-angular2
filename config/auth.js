887f6770dc4cb479d4ed4f919ad91b0ddb185939
Benjamin F <benjamin.fellows@gmail.com>
2017-11-17 02:21:18 +0000

    Changed secret things to placeholder things.

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     'googleAuth': {
-        'clientID': '319315575476-e06r7drd71robn0jgo8i8sto2bnrilo0.apps.googleusercontent.com',
-        'clientSecret': 'lizX3xDUudIjFdFkSKAvv4to',
+        'clientID': 'YOUR_CLIENT_ID',
+        'clientSecret': 'YOUR_CLIENT_SECRET',
         'callbackURL': 'https://bendev-nodejs-surlyp.c9users.io/login/google/callback'
     }
 };
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'BD_NodeJS_App',
-    secret: '11e6b11b-bd2e-4c4f-9240-d0dac9d1eca5',
+    secret: 'YOUR_SECRET',
     port: '6379',
     host: process.env.IP || '127.0.0.1'
 };
